0ebdc5b634d26f5c62f1d659ed350418473c17b2
Parth Tiwari <93487032+parthtiwar-i@users.noreply.github.com>
2023-12-22 17:24:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const input = document.getElementById("searchInput");
 const btn = document.getElementById("search_btn");
+const luckyBtn  = document.getElementById("feeling_lucky");
+
+luckyBtn.addEventListener("click" , ()=>{
+    window.location.href = `https://doodles.google/`;
+})
+
 btn.addEventListener("click" , ()=>{
   const value = input.value.trim();
   if(value !== ""){
